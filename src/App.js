a7092f1ac8138665b979715246a7c605dfb38ae1
Monica Moura <monica.a.moura@hotmail.com>
2018-08-04 04:08:30 +0100

    set state for list of books

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 
 import Search from './Search';
 import Main from './Main';
 
 import './App.css'
 
+
 class BooksApp extends React.Component {
+  state = {
+    books : []
+  }
+
+  componentDidMount () {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books: books })
+    })
+    console.log(this.state.books);
+  }
 
   render() {
     return (
