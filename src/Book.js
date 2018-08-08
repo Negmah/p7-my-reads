1b5a45a41ac34e72148b7ea422d0beed07f6979e
monicaMoura <monica.a.moura@hotmail.com>
2018-08-08 19:46:00 +0100

    Simplify React component and remove this property

--- src/Book.js
+++ src/Book.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 
+/*
 class Book extends Component {
     render() {
-        let bookThumbnail = this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : '';
-
         const { moveShelf, book } = this.props;
+
+        let bookThumbnail = book.imageLinks ? book.imageLinks.thumbnail : '';
 
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${bookThumbnail}` }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${bookThumbnail}"` }}>
+                    </div>
                     <div className="book-shelf-changer">
-                    {/*on click of the button on the cover of the book, call moveShelf method*/}
+                    {/*on click of the button on the cover of the book, call moveShelf method
                     <select
                         onChange = {(event) => {
                           moveShelf(book, event.target.value)}}
@@ -32,5 +34,39 @@ class Book extends Component {
     }
 }
 
+
+export default Book;
+*/
+
+const Book = (props) => {
+
+    const { moveShelf, book } = props;
+    let bookThumbnail = book.imageLinks ? book.imageLinks.thumbnail : '';
+
+    return (
+        <div className="book">
+                <div className="book-top">
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${bookThumbnail}")` }}>
+                    </div>
+                    <div className="book-shelf-changer">
+                    {/*on click of the button on the cover of the book, call moveShelf method*/}
+                    <select
+                        onChange = {(event) => {
+                          moveShelf(book, event.target.value)}}
+                        value = {props.currentShelf}
+                    >
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
+                    </div>
+                </div>
+                <div className="book-title">{props.book.title}</div>
+                <div className="book-authors">{props.book.authors}</div>
+            </div>
+    );
+}
 
 export default Book;
